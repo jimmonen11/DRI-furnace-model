86039ea759f74e333d5eea8c1c95d10289ef502f
jimmonen <jimmonen@PowellDellLap.chemeng.utah.edu>
2023-09-05 15:49:09 -0600

    Initial Commit.

---
+++ DRI_furnace_loader.m
@@ -0,0 +1,92 @@
+n_furnace = 10;
+
+c_H2Oinit = 1*ones(1, n_furnace);
+c_H2init = 12*ones(1, n_furnace);
+%c_H2Oinit = 2*ones(1, n_furnace);
+%c_H2Oinit = 2*ones(1, n_furnace);
+
+c_Fe2O3init = 4750*ones(1, n_furnace);
+c_Fe3O4init = 1e-20*ones(1, n_furnace);
+c_FeOinit = 1e-20*ones(1, n_furnace);
+c_Feinit = 1e-20*ones(1, n_furnace);
+
+T_sinit = (500+273)*ones(1, n_furnace);
+T_ginit = (900+273)*ones(1, n_furnace);
+
+c_H2in = 12;
+c_H2Oin = 1;
+
+%DRIflow = 50.33; %kg/s
+DRIflow = 52; %kg/s
+Reducerflow = 8.4309; %kg/s
+%Reducerflow = 10.685; % kg/s
+
+reducerflowmole = Reducerflow/4 * 1000;
+
+r_furnace = 6.6/2; %m, radius of furnace
+h_furnace = 6; %m, height of furnace
+A_furnace = pi*r_furnace^2; %m^2, c.s. area of flow
+
+V_furnace = A_furnace*h_furnace;
+
+rho_bed = 4750; %kg/m^3, density of bed
+rho_gas = 0.0374; %kg/m^3, density of gas
+
+
+u_s = DRIflow/rho_bed/A_furnace;
+u_g = Reducerflow/rho_gas/A_furnace;
+
+dz = h_furnace/n_furnace;
+
+
+r_p = 6e-3; %m
+
+
+eps_bed = 0.4;
+a_b = 6*(1-eps_bed)/(r_p*2); %m^2/m^3, suface area for gas solid heat exchange
+
+pellet_flow = (3*DRIflow)/(4*pi*r_p^3*rho_bed)
+
+tau_furnace = h_furnace*A_furnace/(DRIflow/rho_bed)
+
+
+n_pellets = pellet_flow*tau_furnace/(n_furnace);
+
+
+rho_pellet = 5275; %kg/m^3, Fe2O3
+V_pellet = (4/3)*pi*r_p^3;
+m_pellet = rho_pellet*V_pellet;
+
+M_Fe2O3 = 159.69; %g/mol
+
+molesO2pellet = 0.63*m_pellet/M_Fe2O3*1000;
+kappa = molesO2pellet%m_pellet;
+
+T = 800+273;
+P = 101325*1.16; %thesis de Costa
+R = 8.314; %(m^3*Pa)/(K*mol)
+ct = P/(R*T)
+
+M_H2 = 2;
+M_H2O = 18;
+
+x_H2in = 0.98;
+x_H2Oin = 1 - x_H2in;
+
+%c_H2in = ct*x_H2in;
+%c_H2Oin = ct*x_H2Oin;
+
+n_gas_in = 3634; %mol/s
+flow = (n_gas_in*M_H2*x_H2in + n_gas_in*M_H2O*x_H2Oin)/1000;
+
+% c_H2Oinit = n_gas_in*x_H2Oin*ones(1, n_furnace);
+% c_H2init = n_gas_in*x_H2Oin*ones(1, n_furnace);
+
+%Actually flows
+% c_H2in = n_gas_in*x_H2in;
+% c_H2Oin = n_gas_in*x_H2Oin;
+% 
+% c_Fe2O3init = 52*ones(1, n_furnace);
+% c_Fe3O4init = 1e-20*ones(1, n_furnace);
+% c_FeOinit = 1e-20*ones(1, n_furnace);
+% c_Feinit = 1e-20*ones(1, n_furnace);
---
+++ USCM.m
@@ -0,0 +1,87 @@
+function [V1, V2, V3] = USCM(X1, X2, X3, r0, T, c_H2, ct, V_node, n_pellets)
+
+%% Clean Up
+
+%Makes sure X values are positive
+if X1 < 1e-3
+    X1 = 1e-10;
+end
+
+if X2 < 1e-3
+    X2 = 1e-10;
+end
+
+if X3 < 1e-3
+    X3 = 1e-10;
+end
+
+%% Effective Diffusion Relations
+%From:
+%S. Yu, L. Shao, Z. Zou, and H. Saxén, “A numerical study on the performance of the h2 shaft furnace with dual-row top gas recycling,” Processes, vol. 9, no. 12, 2021, doi: 10.3390/pr9122134.
+%which references:
+%R. Takahashi, Y. Takahashi, J. Yagi, and Y. Omori, “Operation and Simulation of Pressurized Shaft Furnace for Direct Reduction,” Trans. Iron Steel Inst. Japan, vol. 26, no. 9, pp. 765–774, 1986.
+D_H2_1 = exp(3.43 - 4.2e3/T)/100^2; %m^2/s
+D_H2_2 = exp(5.65 - 6.8e3/T)/100^2; %m^2/s
+D_H2_3 = exp(4.77 - 5.9e3/T)/100^2;%m^2/s
+
+%kf = 0.01; %m/s
+kf = 0.47; %m/s
+
+D_H2 = 4.767751e-5;
+
+%% Equilibrium Constants and Parameters
+%From "The Effect of Gas and Solids Maldistribution on the Performance of
+%Moving-bed Reactors: The Reduction of Iron Oxide Pelletswith Hydrogen"
+%T in Kelvin
+K1 = exp((362/T)+10.32);
+K2 = exp((-8580/T)+8.98);
+K3 = exp((-2070/T)+1.30);
+
+
+c_H2eq1 = (1/(K1+1))*ct;
+c_H2eq2 = (1/(K2+1))*ct;
+c_H2eq3 = (1/(K3+1))*ct;
+
+
+%% Reaction rate Coefficient
+R = 8.314; %J/mol*K
+
+%k1 = 7.79e-7*exp(-27000/(R*T))
+%k2 = 1.11e-2*exp(-55000/(R*T))
+%k3 = 16*exp(-136000/(R*T))
+
+%wagner thesis m/s
+k1 = 6.3587e-2*exp(-43276/(R*T));
+k2 = 1.1290e-3*exp(-21570/(R*T));
+k3 = 3.8337e-2*exp(-50129/(R*T));
+
+
+% k1 = 1.44e5*exp(-6650/T)/3600;
+% k2 = 2.88e5*exp(-8580/T)/3600;
+% k3 = 2.45e7*exp(-1400/T)/3600;
+
+
+%%
+A1 = 1/(X1^2*(k1*(1+1/K1)));
+A2 = 1/(X2^2*(k2*(1+1/K2)));
+A3 = 1/(X3^2*(k3*(1+1/K3)));
+
+B1 = (X2-X1)*r0/(D_H2*X1*X2);
+B2 = (X3-X2)*r0/(D_H2*X2*X3);
+B3 = (1-X3)*r0/(D_H2*X3);
+
+F = 1/kf;
+
+W1 = (A1+B1)*(A3*(A2+B2+B3+F) + (A2+B2)*(B3+F)) + A2*(A3*(B2+B3+F) + B2*(B3+F));
+
+%%
+
+V1 = (4*pi*r0^2*( ((A3*(A2+B2+B3+F)) + (A2+B2)*(B3+F))*(c_H2-c_H2eq1)...
+    - (B3+F)*A2*(c_H2-c_H2eq3) - (A3*(B2+B3+F)+B2*(B3+F))*(c_H2-c_H2eq2)) / W1);
+
+V2 = (4*pi*r0^2*( ((A1+B1+B2)*(A3+B3+F) + A3*(B3+F))*(c_H2-c_H2eq2)...
+    - (B2*(A3+B3+F)+(A3*(B3+F)))*(c_H2-c_H2eq1) - ((A1+B1)*(B3+F))*(c_H2-c_H2eq3)) / W1);
+
+V3 = (4*pi*r0^2*( ((A1+B1)*(A2+B2+B3+F) + A2*(B2+B3+F))*(c_H2-c_H2eq3)...
+    - (A2*(B3+F))*(c_H2-c_H2eq1) - ((A1+B1)*(B3+F))*(c_H2-c_H2eq2)) / W1);
+
