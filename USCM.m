3975bbdd721665f9f60723bfbfc7ca622aa1d5b2
jimmonen <jimmonen@xps10.chemeng.utah.edu>
2023-11-29 15:35:48 -0700

    Updated K values with Gibbs relation.

--- USCM.m
+++ USCM.m
@@ -3,42 +3,53 @@ function [V1, V2, V3] = USCM(X1, X2, X3, r0, T, c_H2, ct, rxns)
 %% Clean Up
 
 % %Makes sure X values are positive
-if X1 < 1e-5
-    X1 = 1e-5;
-end
-
-if X2 < 1e-5
-    X2 = 1e-5;
-end
-
-if X3 < 1e-5
-    X3 = 1e-5;
-end
+% if X1 < 1e-5
+%     X1 = 1e-5;
+% end
+% 
+% if X2 < 1e-5
+%     X2 = 1e-5;
+% end
+% 
+% if X3 < 1e-5
+%     X3 = 1e-5;
+% end
 
 %% Effective Diffusion Relations
 %From:
 %S. Yu, L. Shao, Z. Zou, and H. Saxén, “A numerical study on the performance of the h2 shaft furnace with dual-row top gas recycling,” Processes, vol. 9, no. 12, 2021, doi: 10.3390/pr9122134.
 %which references:
 %R. Takahashi, Y. Takahashi, J. Yagi, and Y. Omori, “Operation and Simulation of Pressurized Shaft Furnace for Direct Reduction,” Trans. Iron Steel Inst. Japan, vol. 26, no. 9, pp. 765–774, 1986.
-%D_H2_1 = exp(3.43 - 4.2e3/T)/100^2; %m^2/s
-%D_H2_2 = exp(5.65 - 6.8e3/T)/100^2; %m^2/s
-%D_H2_3 = exp(4.77 - 5.9e3/T)/100^2;%m^2/s
+D_H2_1 = exp(3.43 - 4.2e3/T)/100^2; %m^2/s
+D_H2_2 = exp(5.65 - 6.8e3/T)/100^2; %m^2/s
+D_H2_3 = exp(4.77 - 5.9e3/T)/100^2;%m^2/s
 
 %kf = 0.01; %m/s
 kf = 0.47; %m/s
 
-D_H2_1 = 4.767751e-5;
-D_H2_2 = 4.767751e-5;
-D_H2_3 = 4.767751e-5;
+% D_H2_1 = 4.767751e-5;
+% D_H2_2 = 4.767751e-5;
+% D_H2_3 = 4.767751e-5;
 
 
 %% Equilibrium Constants and Parameters
 %From "The Effect of Gas and Solids Maldistribution on the Performance of
 %Moving-bed Reactors: The Reduction of Iron Oxide Pelletswith Hydrogen"
 %T in Kelvin
-K1 = exp((362/T)+10.32);
-K2 = exp((-8580/T)+8.98);
-K3 = exp((-2070/T)+1.30);
+R = 8.314; %J/mol*K
+
+G1 = -155547 - 74.7*(T);
+G2 = 71490 - 73.12*(T);
+G3 = 23430 - 16.16*(T);
+
+K1 = exp(-G1/(R*T));
+K2 = exp(-G2/(R*T));
+K3 = exp(-G3/(R*T));
+
+
+% K1 = exp((362/T)+10.32);
+% K2 = exp((-8580/T)+8.98);
+% K3 = exp((-2070/T)+1.30);
 
 
 c_H2eq1 = (1/(K1+1))*ct;
@@ -47,7 +58,6 @@ c_H2eq3 = (1/(K3+1))*ct;
 
 
 %% Reaction rate Coefficient
-R = 8.314; %J/mol*K
 
 %k1 = 7.79e-7*exp(-27000/(R*T));
 %k2 = 1.11e-2*exp(-55000/(R*T));
