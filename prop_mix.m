dc2c2489eb0ff0b01720991d3e00f3975960cee9
jimmonen <jimmonen@pow13.chemeng.utah.edu>
2023-12-19 10:22:50 -0700

    Added heat of reaction values to Simulink model using regression fits. Updated prop_grab comments.

--- prop_mix.m
+++ prop_mix.m
@@ -1,9 +1,9 @@
-function [rho, mu, k, Cp] = prop_mix(rhos, mus, ks, Cps, x_fracs)
+function [rho, mu, k, Cp] = prop_mix(rhos, mus, ks, Cps, w_fracs)
 
 %Function that gives weighted average of properties
 
-%using mole weighted average because gas properties
-rho = dot(rhos, x_fracs);
-mu = dot(mus,x_fracs);
-k = dot(ks,x_fracs);
-Cp = dot(Cps, x_fracs);
+%using mass weighted average
+rho = dot(rhos, w_fracs);
+mu = dot(mus,w_fracs);
+k = dot(ks,w_fracs);
+Cp = dot(Cps, w_fracs);
